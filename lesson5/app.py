e017ea13adaeb1f5c4bf6b849d695143179b8a46
Praiseandzoeventures <onyfaith@gmail.com>
2026-09-13 15:18:14 +0100

    hardwork pays

---
+++ lesson5/app.py
@@ -0,0 +1,35 @@
+@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET','POST'])
+def register():
+    msg =""
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        mydb = mysql.connector.connect(
+
+host='remoteysql.com',
+
+user='Rz8hqnlk4',
+
+password='nd6wK03xe0',
+
+database='Rz8hqnlk4'
+
+)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    return render_template('register.html', msg=msg)
